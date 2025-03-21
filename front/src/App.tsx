5ca174cb7e8a4149be79812793b6785f0eb7c6a0
ahamie71 <ahmad.hamieh2002@hotmail.com>
2025-03-21 14:43:17 +0100

    frontend fisrt commit

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
 import { NewsPage } from './pages/NewsPage';
 import { ProtectedRoute } from './components/ProtectedRoute';
-
+import PopularityPage from './pages/PopularityPage';
+import HomepagePage from './pages/HomePage';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Page de connexion (accessible sans protection) */}
         <Route path="/login" element={<LoginPage />} />
+
+        {/* Pages protégées */}
         <Route
           path="/"
           element={
@@ -16,6 +20,28 @@ function App() {
             </ProtectedRoute>
           }
         />
+        <Route
+          path="/popularite"
+          element={
+            <ProtectedRoute>
+              <PopularityPage />
+            </ProtectedRoute>
+          }
+
+        />
+         <Route
+          path="/Home"
+          element={
+            <ProtectedRoute>
+              <HomepagePage />
+            </ProtectedRoute>
+          }
+
+        />
+        
+        
+
+        {/* Redirection pour les routes inconnues */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
