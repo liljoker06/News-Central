8f34ee72b9efb9ccfd0481940ecdce96cdbe0765
Batyeste <batystebatybat@gmail.com>
2025-03-21 16:13:20 +0100

    historique + components | header / fu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,22 +1,26 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
 import { NewsPage } from './pages/NewsPage';
+import { HistoryPage } from './pages/HistoryPage';
 import { ProtectedRoute } from './components/ProtectedRoute';
+import { Header } from "./components/Header"
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
+
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="/"
           element={
-            <ProtectedRoute>
+
               <NewsPage />
-            </ProtectedRoute>
           }
         />
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="history" element={<HistoryPage />} />
       </Routes>
     </BrowserRouter>
   );
